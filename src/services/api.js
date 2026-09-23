4669c6b2947385f7e0183d2dc3e5ce5054502f15
Anatoliy <akonst008@gmail.com>
2026-09-23 10:16:13 +0300

    branch fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,72 +1,71 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API_URL = 'https://wedev-api.sky.pro/api/kanban'
+const API_URL = "https://wedev-api.sky.pro/api/kanban";
 export async function fetchWords({ token }) {
-   try {
-      const data = await axios.get(API_URL, {
-         headers: {
-            Authorization: 'Bearer ' + token,
-         },
-      })
-      return data.data
-      // когда работаем с axios, не забываем, что результат лежит в ключе datа
-   } catch (error) {
-      throw new Error(error.message)
-   }
+  try {
+    const data = await axios.get(API_URL, {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+    return data.data;
+    // когда работаем с axios, не забываем, что результат лежит в ключе datа
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
 
 export async function getWord({ token, id }) {
-   try {
-      const data = await axios.get(`${API_URL}/${id}`, {
-         headers: {
-            Authorization: 'Bearer ' + token,
-         },
-      })
-      return data.data
-   } catch (error) {
-      throw new Error(error.message)
-   }
+  try {
+    const data = await axios.get(`${API_URL}/${id}`, {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+    return data.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
 
 export async function postWord({ token, word }) {
-   try {
-      const data = await axios.post(`${API_URL}`, word, {
-         headers: {
-            Authorization: 'Bearer ' + token,
-            'Content-Type': 'text/html',
-         },
-      })
-      return data.data.tasks
-   } catch (error) {
-      throw new Error(error.message)
-   }
+  try {
+    const data = await axios.post(`${API_URL}`, word, {
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "text/html",
+      },
+    });
+    return data.data.tasks;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
 
 export async function editWord({ token, id, word }) {
-    try {
-        const data = await axios.put(`${API_URL}/${id}`, word, {
-            headers: {
-                Authorization: 'Bearer ' + token,
-            'Content-Type': 'text/html',
-        },
-        })
-        return data.data.tasks
-    } catch (error) {
-        throw new Error(error.message)
-    }
+  try {
+    const data = await axios.put(`${API_URL}/${id}`, word, {
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "text/html",
+      },
+    });
+    return data.data.tasks;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
 
-
-export async function deleteWord({ token, id }) { 
-    try {
-        const data = await axios.delete(`${API_URL}/${id}`, { 
-            headers: {
-                Authorization: "Bearer " + token,
-                "Content-Type":"text",
-            },
-        });
-        return data.data.words;
-    } catch (error) {
-        throw new Error(error.message);
-      }
-};
+export async function deleteWord({ token, id }) {
+  try {
+    const data = await axios.delete(`${API_URL}/${id}`, {
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "text",
+      },
+    });
+    return data.data.words;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
